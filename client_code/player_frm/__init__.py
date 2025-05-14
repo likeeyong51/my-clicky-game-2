ed2621193b26aa16affde0ccd2bbe1442ab589df
likeeyong17@gmail.com <likeeyong17@gmail.com>
2025-05-14 04:02:04 +0000

    Created player; Edited player_frm and game_frm

--- client_code/player_frm/__init__.py
+++ client_code/player_frm/__init__.py
@@ -1,8 +1,10 @@
 from ._anvil_designer import player_frmTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+# from anvil import Notification
 from ..game_frm import game_frm
 
 class player_frm(player_frmTemplate):
@@ -11,16 +13,21 @@ class player_frm(player_frmTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+        print('player form')
 
     def load_btn_click(self, **event_args):
         """This method is called when the button is clicked"""
         if self.player_txb.text:
+            # if not anvil.server.call('add_player', player_name=self.item['player']):
+            #     Notification('Add player failed').show()
             # clear old
             self.game_pnl.clear()
             # load game form
             self.game_pnl.add_component(game_frm(player=self.item['player'].title()))
             # show game panel
             self.game_pnl.visible= True
+        # else:
+        #     Notification('Please enter your name').show()
 
     def hide_player_card(self, show):
         self.player_crd.visible = show
---
+++ server_code/player.py
@@ -0,0 +1,32 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def add_player(player_name):
+    '''adds a new player to the player table'''
+    # check if the player exists in the player table
+    name = app_tables.player.search(player=player_name)
+    
+    if len(name) == 1:
+        return False # the player exists
+        
+    if app_tables.player.add_row(player=player_name):
+        return True # added successfully
+
+    return False # add player failed
+        
